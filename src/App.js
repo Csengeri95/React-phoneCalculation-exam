4bc06cb1602f8e223c8cc0f82758a87d5d2094ff
Csengeri95 <109845880+Csengeri95@users.noreply.github.com>
2022-12-26 15:06:58 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import './App.css';
+import { BrowserRouter as Router } from "react-router-dom"
+import Header from './components/Header';
+import Content from './components/Content';
+import Products from './pages/Products';
+import Calculation from './pages/Calculation';
+import Home from "./pages/Home"
+import Footer from './components/Footer';
+import PageNotFound from './pages/PageNotFound';
+import { useEffect, useState } from 'react';
+import { GetproductsContext, GetproductsContextDefaults } from './contexts/GetproductsContext';
+
+const config = require("../package.json")
+
+function App() {
+
+  const [getProducts, setGetProducts] = useState(GetproductsContextDefaults.value)
+
+
+
+  const pages = [
+    { name: "Homepage", path: "/", element: <Home />, status: true },
+    { name: "Products", path: "/products", element: <Products />, status: true },
+    { name: "Calculation", path: "/calculation", element: <Calculation />, status: true },
+    { name: "PageNotFound", path: "/*", element: <PageNotFound />, status: false }
+  ]
+
+  useEffect(() => {
+    document.title = config.title
+  })
+
+  return (
+    <GetproductsContext.Provider value={{ getProducts, setGetProducts }}>
+
+      <div className="App">
+        <Router>
+
+          <Header menu={pages} />
+          <Content route={pages} />
+          <Footer />
+        </Router>
+
+
+      </div>
+
+    </GetproductsContext.Provider>
+
+  );
+}
+
+export default App;
